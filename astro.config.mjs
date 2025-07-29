dbc2b60ab404b0c127764cdb8eb8685db1c9f356
lovebirds-world <lovebirds-world@proton.me>
2025-07-29 16:27:44 +0200

    ñkjhñk

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    assets: "astro",
-  },
   site: "https://lovebirds-world.online",
   integrations: [
     tailwind(),
